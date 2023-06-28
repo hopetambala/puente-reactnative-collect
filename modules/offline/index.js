b70cc9225c928c90b33162b80b2bbb88ce417086
Hope Tambala <hopetambala@outlook.com>
2023-06-28 09:54:57 -0400

    Revert "Adds easytimer dependency and logs timing of checkOnlineStatus "

--- modules/offline/index.js
+++ modules/offline/index.js
@@ -1,30 +1,16 @@
 import NetInfo from "@react-native-community/netinfo";
 import * as Network from "expo-network";
 import { Platform } from "react-native";
-import Timer from "easytimer";
-import getAWSLogger from "@modules/aws-logging/logger";
 
 // checks whether user is connected to internet, return true if connected, false otherwise
-const checkOnlineStatus = () => {
-  let timer = new Timer();
-  timer.start({precision: 'seconds'});
-  return new Promise((resolve, reject) => {
+const checkOnlineStatus = () =>
+  new Promise((resolve, reject) => {
     if (Platform.OS === "ios") {
       Network.getNetworkStateAsync().then(
         (status) => {
-          timer.stop();
-          getAWSLogger().log({
-            type: "CHECK_ONLINE_STATUS_SUCCESS_TIMER_SUCCESS",
-            seconds: timer.getTimeValues().toString(),
-          });
           resolve(status.isConnected);
         },
         (error) => {
-          timer.stop();
-          getAWSLogger().log({
-            type: "CHECK_ONLINE_STATUS_SUCCESS_TIMER_ERROR",
-            seconds: timer.getTimeValues().toString(),
-          });
           reject(error);
         }
       );
@@ -37,18 +23,8 @@ const checkOnlineStatus = () => {
             state.details.strength !== undefined &&
             state.details.strength > 10
           ) {
-            timer.stop();
-            getAWSLogger().log({
-              type: "CHECK_ONLINE_STATUS_SUCCESS_TIMER_SUCCESS",
-              seconds: timer.getTimeValues().toString(),
-            });
             resolve(true);
           } else {
-            timer.stop();
-            getAWSLogger().log({
-              type: "CHECK_ONLINE_STATUS_SUCCESS_TIMER_ERROR",
-              seconds: timer.getTimeValues().toString(),
-            });
             resolve(false);
           }
         },
@@ -58,5 +34,5 @@ const checkOnlineStatus = () => {
       );
     }
   });
-}
+
 export default checkOnlineStatus;
